59f65caeef11389b08f0f8e3765edc99ccac2528
CarstenRuetz <carsten.ruetz@me.com>
2023-04-29 15:29:43 +0200

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>Test {{ test }}</template>
+
+<script setup>
+import Store from "@ember-data/store";
+import Cache from "@ember-data/json-api";
+
+class EmberStore extends Store {
+    createCache(storeWrapper) {
+        return new Cache(storeWrapper);
+    }
+}
+
+const test = "123";
+</script>
